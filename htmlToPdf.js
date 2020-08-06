696d5ff9953889808cc78acebe2989c38f98b122
404391045@qq.com <nandy@zhizhangyi.com>
2020-08-06 10:59:07 +0800

    增加实例池

--- htmlToPdf.js
+++ htmlToPdf.js
@@ -1,12 +1,29 @@
 const puppeteer = require('puppeteer-core');
+const initPuppeteerPool = require('./puppeteer-pool');
 const utils = require('./utils');
 
-module.exports = async function (opts) {
+const pool = initPuppeteerPool({ // 全局只应该被初始化一次
+    // puppeteerArgs: {
+    //   ignoreHTTPSErrors: true,
+    //   headless: false, // 是否启用无头模式页面
+    //   timeout: 0,
+    //   pipe: true, // 不使用 websocket 
+    // }
+    puppeteerArgs: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        executablePath: utils.chrome(),
+        ignoreHTTPSErrors: true
+    }
+});
+
+const single = async function (opts) {
 
     const { url, savePath, waitTime, pdfOpts = {}, styleContent } = opts || {};
 
+    let browser;
+
     try {
-        const browser = await puppeteer.launch({
+        browser = await puppeteer.launch({
             args: ['--no-sandbox', '--disable-setuid-sandbox'],
             executablePath: utils.chrome(),
             ignoreHTTPSErrors: true
@@ -40,8 +57,54 @@ module.exports = async function (opts) {
         await browser.close();
     } catch (e) {
         console.error(e);
+        if(browser) await browser.close();
         return false;
     }
 
     return true;
 };
+
+
+const muti = async function(opts) {
+
+    const { url, savePath, waitTime, pdfOpts = {}, styleContent } = opts || {};
+    
+    try{
+        const page = await pool.use(async function (instance){
+            const page = await instance.newPage();
+            return page;
+        });
+    
+        await page.goto(url, { waitUntil: 'networkidle0' });
+
+        if (waitTime) {
+            await page.waitFor(waitTime);
+        }
+
+        if (styleContent) {
+            await page.addStyleTag({
+                content: styleContent
+            });
+        }
+
+        await page.emulateMedia('print');
+
+        await page.pdf(Object.assign({
+            path: savePath,
+            format: 'A4',
+            scale: 1,
+            printBackground: true,
+            landscape: false,
+            displayHeaderFooter: false,
+        }, pdfOpts));
+    }catch(e){
+        console.log(e);
+        return false;
+    }
+    
+    return true;
+};
+
+
+
+module.exports = single;
